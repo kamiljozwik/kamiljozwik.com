aed27bc89f8573f9768805b2495a7ac31302d455
Kamil Jozwik <jozwikk@vitessepsp.com>
2025-04-07 14:16:28 +0200

    wording changes

--- src/app/pl/offer/components/ServicesSection.tsx
+++ src/app/pl/offer/components/ServicesSection.tsx
@@ -14,25 +14,25 @@ export const ServicesSection = () => {
       icon: <Code className="h-10 w-full text-primary" />,
       title: 'Aplikacje webowe',
       description:
-        'Tworzymy responsywne, wydajne i zorientowane na użytkownika aplikacje internetowe, które angażują odbiorców i realizują cele biznesowe.',
+        'Rozbudowane, wydajne i nowoczesne aplikacje internetowe, które pozwalają na rozwój Twojego biznesu.',
     },
     {
       icon: <Code className="h-10 w-full text-primary" />,
       title: 'Strony internetowe',
       description:
-        'Tworzymy responsywne, wydajne i zorientowane na użytkownika aplikacje internetowe, które angażują odbiorców i realizują cele biznesowe.',
+        'Responsywne, szybkie i zorientowane na użytkownika aplikacje internetowe, które angażują odbiorców i realizują cele biznesowe.',
     },
     {
       icon: <Smartphone className="h-10 w-full text-primary" />,
       title: 'Aplikacje mobilne',
       description:
-        'Projektujemy i budujemy intuicyjne aplikacje na platformy iOS i Android, dostarczając realną wartość użytkownikom w ich codziennym życiu.',
+        'Intuicyjne aplikacje na platformy iOS i Android, dostarczające realną wartość użytkownikom w ich codziennym życiu.',
     },
     {
       icon: <BrainCircuit className="h-10 w-full text-primary" />,
       title: 'Automatyzacje AI',
       description:
-        'Optymalizujemy procesy biznesowe, usprawniamy podejmowanie decyzji i tworzymy inteligentne rozwiązania dzięki niestandardowym automatyzacjom opartym na AI.',
+        'Optymalizacja procesów biznesowych, usprawnianie podejmowanych decyzji i tworzenie inteligentnych rozwiązań dzięki niestandardowym automatyzacjom opartym na AI.',
     },
   ];
 
@@ -44,8 +44,7 @@ export const ServicesSection = () => {
         </h2>
         <p className="w-2/3 text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-balance">
           Dostarczam kompleksowe rozwiązania cyfrowe dopasowane do Twoich
-          unikalnych potrzeb. Skupiamy się na jakości, wydajności i
-          innowacyjności.
+          unikalnych potrzeb.
         </p>
       </div>
       <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl">
